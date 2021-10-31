4c8477abe72b7ae32564b4b36eb573869a3d9328
Samikshya <write2samikshyakar@gmail.com>
2021-10-31 10:01:33 +0530

    Add project files.

---
+++ WpfApp1frmBookBon1/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1frmBookBon1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_click(object sender, RoutedEventArgs e)
+        {
+            string name = txtName.Text.Trim();
+            string Job = txtJob1.Text.Trim();
+
+            if (name.Length > 0 && Job.Length > 0)
+            {
+                string text = string.Format("Hello {0}\n your Job is {1}\n : Thank you ", name,Job );
+
+                MessageBox.Show(text,"Information" );
+
+                txtName.Clear();
+               
+                txtName.Focus();
+               
+
+            }
+
+            else MessageBox.Show("Please Enter the Name and Job Fields");
+        }
+    }
+}
---
+++ WpfApp1frmBookBon1/Window1.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1frmBookBon1
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void btnclick_click(object sender, RoutedEventArgs e)
+        {
+            string yourname = txtName1.Text.Trim();
+            string yourjob = txtJob.Text.Trim();
+            if (yourname.Length > 0 && yourjob.Length > 0)
+            {
+
+                string msg = string.Format("Hello {0} \n your Job Is {1} : Thank You ", yourname, yourjob);
+
+                MessageBox.Show(msg, "information ");
+                txtName1.Clear();
+                txtJob.Clear();
+                txtName1.Focus();
+
+            }
+
+            else MessageBox.Show("Enter name and Job " , "Error");
+        }
+    }
+}
